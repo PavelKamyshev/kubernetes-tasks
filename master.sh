d4e2022e8302ce2ae7165a249625dde13a865034
Pavel Kamyshev <Pavel_Kamyshev@epam.com>
2017-10-12 12:53:33 +0300

    add cerf

--- master.sh
+++ master.sh
@@ -13,19 +13,32 @@ apt-get -y update && apt-get install -y git openjdk-8-jre docker.io kubelet kube
 #kubeadm reset && kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=$(ifconfig enp0s9|xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/') > /vagrant/kube.txt #--skip-preflight-checks 
 #sleep 10
 
-#su - ubuntu | mkdir -p /home/ubuntu/.kube && sudo cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config && sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
+# $ mkdir -p /home/ubuntu/.kube && sudo cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config && sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config
 #sleep 3
+## сразу после установки зайти и выполнить
+# $ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.8.0/Documentation/kube-flannel.yml
+# $ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.8.0/Documentation/kube-flannel-rbac.yml
+# $ kubectl create -f /vagrant/kube-dashboard-rbac.yml
+# генерируем приватные ключи
+# $ mkdir certs
+# $ cd certs
+# $ openssl genrsa -des3 -passout pass:x -out dashboard.pass.key 2048
+# $ openssl rsa -passin pass:x -in dashboard.pass.key -out dashboard.key
+# $ rm dashboard.pass.key
+# $ openssl req -new -key dashboard.key -out dashboard.csr ###Отвечаем там на вопросы
+# $ openssl x509 -req -sha256 -days 365 -in dashboard.csr -signkey dashboard.key -out dashboard.crt
+# $ kubectl create secret generic kubernetes-dashboard-certs --from-file=$HOME/certs -n kube-system
+# $ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
+# правим конфиг дашборда
+# $ kubectl -n kube-system edit service kubernetes-dashboard
+# меняем 
+# type: ClusterIP
+# на
+# type: NodePort
+# сохраняем :esc :wq
+# смотрим на каком порту крутится теперь сервис dashboard
+# $ kubectl -n kube-system get service
 
-# сразу после установки зайти и выполнить
-# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.8.0/Documentation/kube-flannel.yml
-#
-# kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.8.0/Documentation/kube-flannel-rbac.yml
-# перед применением дашборда ввыполняем команду kubectl create -f /vagrant/kube-dashboard-rbac.yml
-# потом  kubectl apply -f /vagrant/kube-dashboard-rbac.yml потом применяем дашборд
-# kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/alternative/kubernetes-dashboard.yaml # применяем дашбоард
-# kubectl proxy --disable-filter=true --address $(ifconfig enp0s8|xargs|awk '{print $7}'|sed -e 's/[a-z]*:/''/') --port=9090 –accept-hosts='^10.19.12.*$'
-#
-#
 #
 #kube-dashboard-rbac.yml
 #apiVersion: rbac.authorization.k8s.io/v1beta1
